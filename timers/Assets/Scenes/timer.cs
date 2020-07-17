237699c83dee595c24e104422fc498ac347b03ab
Medinal305 <medinal305@gmail.com>
2020-07-16 22:02:46 -0600

    UnityJeugo
    ....

--- timers/Assets/Scenes/timer.cs
+++ timers/Assets/Scenes/timer.cs
@@ -5,19 +5,20 @@ using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
-    public text contador;
     private float tiempo = 10f;
+    public Text contadorText;
+
     // Start is called before the first frame update
     void Start()
     {
-        contador = "  " + tiempo;
+        contadorText.text = "  " + tiempo;
     }
 
     // Update is called once per frame
     void Update()
     {
-        tiempo -= time.deltaTime;
+        tiempo -= Time.deltaTime;
 
-        contador = "  " + tiempo.ToString ("f0");
+        contadorText.text = "  " + tiempo.ToString ("f0");
     }
 }
